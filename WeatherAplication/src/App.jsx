fecd52d5bcfb2330af74d0da634b1ac036f182e0
chrisRV15 <christopher.d.thomasvillalobos@lions.enc.edu>
2024-10-18 00:37:54 -0600

    UI Finish

--- WeatherAplication/src/App.jsx
+++ WeatherAplication/src/App.jsx
@@ -1,18 +1,12 @@
 import { useState } from 'react'
 import './styles.css';
+import windimage from './assets/wind.png';
+import humidityimage from './assets/humidity.png';
+import thermometerimage from './assets/thermometer.png';
+import visibiltyimage from './assets/visibility.png';
 
 
 export default function App() {
-  return (
-    <>
-    <div>
-      <Weather />
-    </div>
-    </>
-  );
-}
-
-function Weather() {
   return (
     <>
     <div className='weather'>
@@ -21,19 +15,44 @@ function Weather() {
         <button type="submit">Search</button>
       </div>
       <div className='weather-data'>
-        <img className="weather-icon" alt="Weather icon" />
         <h1 className="cityName">New York</h1>
+        <img className="weather-icon" alt="Weather icon" />
         <h2 className="temperature"> 20&deg;C</h2>
+      <div className='weather-info'>
           <div className='col'>
-          <p className="humidity">humidity</p>
-          <p className="windSpeed">wind speed</p>
-          <p className="feelsLike">feels like</p>
-          <p className="clouds">clouds</p>
+            <div>
+              <img src={humidityimage} alt=""/>
+              <p>91%</p>
+              <span>Humidity</span>
+            </div>
           </div>
+          <div className='col'>
+            <div>
+              <img src={windimage} alt=""/>
+              <p>91%</p>
+              <span>Wind</span>
+            </div>
+          </div>
+          <div className='col'>
+            <div>
+              <img src={thermometerimage} alt=""/>
+              <p>25&deg;C</p>
+              <span>Feel like</span>
+            </div>
+          </div>
+          <div className='col'>
+            <div>
+              <img src={visibiltyimage} alt=""/>
+              <p>20%</p>
+              <span>Visibility</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     </>
   );
 }
+
 
 
